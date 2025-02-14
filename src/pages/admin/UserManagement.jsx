edfec97fbcd1133635c332cc01d79d57d0c684c7
Monishka <monishka0709@gmail.com>
2025-02-14 23:30:36 +0530

    Update UserManagement.jsx

--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import { Avatar, Skeleton } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+import Table from "../../components/shared/Table";
+import { transformImage } from "../../lib/features";
 
+
+
+const columns = [
+  {
+  field: "id",
+  headerName: "ID",
+  headerClassName: "table-header",
+  width: 200,
+},
+{
+  field: "avatar",
+  headerName: "Avatar",
+  headerClassName: "table-header",
+  width: 150,
+  renderCell: (params) => (
+    <Avatar alt={params.row.name} src={params.row.avatar} />
+  ),
+},
+
+{
+  field: "name",
+  headerName: "Name",
+  headerClassName: "table-header",
+  width: 200,
+},
+{
+  field: "username",
+  headerName: "Username",
+  headerClassName: "table-header",
+  width: 200,
+},
+{
+  field: "friends",
+  headerName: "Friends",
+  headerClassName: "table-header",
+  width: 150,
+},
+{
+  field: "groups",
+  headerName: "Groups",
+  headerClassName: "table-header",
+  width: 200,
+},
+]
 const UserManagement = () => {
+
+  
+
+  const [rows, setRows] = useState([]);
+
+  
   return (
-    <div>
-      fgrfa
-    </div>
+    <AdminLayout>
+      
+      <Table heading={'All Users'} columns={columns} rows={rows}/>
+      
+    </AdminLayout>
+      
+   
   )
 }
 
