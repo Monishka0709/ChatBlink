791395555869a0f4d8201f613a9a48a5a00f543f
Monishka <monishka0709@gmail.com>
2025-02-14 23:25:16 +0530

    Update AdminLogin.jsx

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-// import axios from "axios";
 import toast from "react-hot-toast";
-// import { useDispatch } from "react-redux";
 import { green } from '@mui/material/colors';
 import { Avatar, Button, Container, IconButton, Paper, Stack, TextField, Typography } from '@mui/material'
 import { CameraAlt as CameraAltIcon} from "@mui/icons-material"
@@ -9,17 +7,21 @@ import { VisuallyHiddenInput } from '../../components/styles/StyledComponents';
 import { useFileHandler, useInputValidation, useStrongPassword } from '6pp'
 import { usernameValidator } from '../../utils/validation';
 import Background from '../../assets/login_background.png';
-// import { bgGradient } from "../constants/color";
-// import { server } from "../constants/config";
-// import { userExists } from "../redux/reducers/auth";
+import { Navigate } from 'react-router-dom';
 
+
+const isAdmin = true;
 
 const AdminLogin = () => {
+
+    const secretKey = useStrongPassword("");
 
     const submitHandler = (e) =>{
         e.preventDefault();
         console.log("Submit");
     }
+
+    if(isAdmin) return <Navigate to="/admin/dashboard"/>
   return (
     <div style={{
         backgroundImage:`url(${Background})`,
@@ -51,7 +53,7 @@ const AdminLogin = () => {
                     fontWeight: 500,
                     fontSize: '2rem'
                 }}
-                >Login</Typography>
+                > Admin Login</Typography>
                 <form style={{
                     width: '100%',
                     marginTop:'1rem',
@@ -61,11 +63,18 @@ const AdminLogin = () => {
                 >
 
                 
-                    {/* <TextField required fullWidth label="Username" margin="normal" variant='outlined' value={username.value} onChange={username.changeHandler}></TextField> */}
-                    {/* <TextField required fullWidth label="Password" type='password' margin="normal" variant='outlined' value={password.value} onChange={password.changeHandler}></TextField> */}
-                    {/* { <Button  fullWidth sx={{marginTop: '1rem', backgroundColor:'#287c8b',border:'1px solid #287c8b', /*'&:hover':{border:'1px solid #ebf9fe'}*/}
-                    <Typography textAlign={'center'} mt={'1rem'} >Or</Typography>
-                    <Button  fullWidth sx={{marginTop: '1rem', backgroundColor:'#ebf9fe', border:'1px solid #ebf9fe', color:'#287c8b', '&:hover':{border:'1px solid rgba(40, 124, 139, 0.82)'}}} variant='text'  onClick={() => setIsLogin(!isLogin)}>Sign Up</Button>
+                    <TextField 
+                            required 
+                            fullWidth 
+                            label="Secret Key" 
+                            type='password' 
+                            margin="normal" 
+                            variant='outlined' 
+                            value={secretKey.value} 
+                            onChange={secretKey.changeHandler} 
+                            />
+                    { <Button  fullWidth sx={{marginTop: '1rem', backgroundColor:'#287c8b',border:'1px solid #287c8b', }} variant='contained' color='primary' type='submit'>Login</Button> }
+                    
                 </form>
                 </> 
                             }
